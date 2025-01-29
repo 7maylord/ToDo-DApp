2e8abf9ab939f928f39eb386e53fc926f456b0fd
Olumide Adenigba <Olumideadenigba@gmail.com>
2025-01-29 17:25:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { ethers } from "ethers";
+import abi from "./abi.json";
+
+const CONTRACT_ADDRESS = "0xeaaaa90bb4846984c6b9c7a3284004e6cb718d22";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
